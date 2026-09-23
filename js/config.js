50763a8aa164b81c1f2072f91d8da7534cfbc205
sman15tebo-web <sman15tebo.bos@gmail.com>
2026-09-23 13:12:10 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -22,7 +22,7 @@ function scrollToTop() { window.scrollTo({ top: 0, behavior: 'smooth' }); }
 
 // 1. Buat "Buku Telepon" yang berisi daftar sekolah dan link Backend-nya masing-masing
 const daftarSekolah = {
-    "sman5tebo": "https://script.google.com/macros/s/AKfycbwX0vNiVka6uOtpTrQ4OA7pBMDIj9P3FZABIcDA0brpO6urXdsunVo-wTYSOKmUkYx0/exec",
+    "sie-mpus": "https://script.google.com/macros/s/AKfycbyBF1zoZJS63NHpEjdS_ABLl0ZBi4yGpFMeHPo8wiQZiaTSfFt8P11X7H8UsTCH7M9r/exec",
     "demo": "https://script.google.com/a/macros/admin.sma.belajar.id/s/...../exec"
 };
 
